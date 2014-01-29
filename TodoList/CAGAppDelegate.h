c4bd44bb1cfaf1e287423ec0c0faea86f079423f
Craig Grummitt <craiggrummitt@gmail.com>
2014-01-29 12:44:08 -0500

    Initial Commit

---
+++ TodoList/CAGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CAGAppDelegate.h
+//  TodoList
+//
+//  Created by CraigGrummitt on 29/01/2014.
+//  Copyright (c) 2014 CraigGrummitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TodoList
+//
+//  Created by CraigGrummitt on 29/01/2014.
+//  Copyright (c) 2014 CraigGrummitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CAGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CAGAppDelegate class]));
+    }
+}
